cdf7e4051f1663367cfbae0ca06251f2995d35f7
Eugene Mah <eugenemah@gmail.com>
2023-04-20 12:52:57 -0400

    Add some simple tests
    Some simple tests for the Modality model

--- tests/Feature/ModalityModelTest.php
+++ tests/Feature/ModalityModelTest.php
@@ -5,4 +5,23 @@ test('can create a new modality model', function () {
 
     $m = Modality::factory()->create();
 
+    expect($m->id)->toBeNumeric();
+
+});
+
+test('can add a new modality', function () {
+    $m = new Modality();
+    $m->modality = 'Test Modality';
+    $m->save();
+
+    expect($m->modality)->toEqual('Test Modality');
+});
+
+test('can update a modality', function() {
+    $m = Modality::factory()->create();
+
+    $m->modality = 'Updated modality';
+    $m->update();
+
+    expect($m->modality)->toEqual('Updated modality');
 });
