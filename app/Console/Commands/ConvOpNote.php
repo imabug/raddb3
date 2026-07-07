9fefec51bad17607d14c8f4450aa7c968b6a4fe8
Eugene Mah <eugenemah@gmail.com>
2026-07-07 10:32:27 -0400

    Artisan CLI command to convert op_notes table to JSON op_notes in machines table

---
+++ app/Console/Commands/ConvOpNote.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Machine;
+use App\Models\OpNote;
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('raddb:conv-op-note')]
+#[Description('Convert operational notes to JSON')]
+class ConvOpNote extends Command
+{
+    /**
+     * Artisan CLI command to take operation notes in the
+     * op_notes table and convert them to JSON that will
+     * be stored in the op_notes column in the machines table.
+     */
+    public function handle()
+    {
+        $machines = Machine::get();
+
+        foreach ($machines as $m) {
+            if ($m->opnote->count() > 0) {
+                $m->op_notes = $m->opnote->pluck('note')->toJson();
+                $this->info('Opnotes for machine ' . $m->description . ' converted');
+                $m->save();
+            }
+        }
+    }
+}
