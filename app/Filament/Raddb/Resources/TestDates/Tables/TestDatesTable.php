09dbd73c60282575559ec1d561dac9510935e24f
Eugene Mah <eugenemah@gmail.com>
2026-07-21 14:59:24 -0400

    Add additional filters to test date table

--- app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
+++ app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
@@ -10,6 +10,7 @@ use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -33,14 +34,24 @@ class TestDatesTable
             ->filters([
                 TrashedFilter::make(),
                 Filter::make('pending')
-                ->label('Pending')
-                ->query(fn(Builder $query): Builder => $query->activeMachines()->pending())
-                ->toggle()
-                ->default(),
+                    ->label('Pending')
+                    ->query(fn(Builder $query): Builder => $query->activeMachines()->pending())
+                    ->toggle()
+                    ->default(),
                 Filter::make('activeMachines')
-                ->label('Active machines')
-                ->query(fn(Builder $query): Builder => $query->activeMachines())
-                ->toggle(),
+                    ->label('Active machines')
+                    ->query(fn(Builder $query): Builder => $query->activeMachines())
+                    ->toggle(),
+                Filter::make('prevYear')
+                    ->label('Previous year')
+                    ->query(fn(Builder $query): Builder => $query->year(date('Y') - 1))
+                    ->toggle(),
+                Filter::make('currYear')
+                    ->label('Current year')
+                    ->query(fn(Builder $query): Builder => $query->year(date('Y')))
+                    ->toggle(),
+                SelectFilter::make('testType')
+                    ->relationship('testType', 'test_type'),
             ])
             ->deferFilters(false)
             ->recordActions([
