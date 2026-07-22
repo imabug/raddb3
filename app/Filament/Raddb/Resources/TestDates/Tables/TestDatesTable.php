c565f365028d216ea0648efbe30fd1a759bfbf03
Eugene Mah <eugenemah@gmail.com>
2026-07-22 09:42:32 -0400

    Add TODO comment for prevYear/currYear filters

--- app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
+++ app/Filament/Raddb/Resources/TestDates/Tables/TestDatesTable.php
@@ -9,6 +9,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
 use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
@@ -43,6 +44,9 @@ class TestDatesTable
                     ->label('Active machines')
                     ->query(fn(Builder $query): Builder => $query->activeMachines())
                     ->toggle(),
+                // TODO: Toggle prevYear/currYear filters depending on the state of the other filter
+                // i.e. if prevYear is toggled on, toggling currYear on should toggle prevYear off but
+                // leave prevYear if it's already off.
                 Filter::make('prevYear')
                     ->label('Previous year')
                     ->query(fn(Builder $query): Builder => $query->year(date('Y') - 1))
@@ -54,27 +58,27 @@ class TestDatesTable
                 SelectFilter::make('testType')
                     ->relationship('testType', 'test_type'),
                 Filter::make('surveyDateRange')
-                ->label('Survey date range')
-                ->schema([
-                    DatePicker::make('surveyStart')
-                        ->label('Survey start date'),
-                    DatePicker::make('surveyEnd')
-                        ->label('Survey end date'),
-                ])
-                ->query(function (Builder $query, array $data): Builder {
-                    return $query
-                        ->when(
-                            $data['surveyStart'],
-                            fn(Builder $query, $date): Builder => $query->whereDate('test_date', '>=', $date),
-                        )
-                        ->when(
-                            $data['surveyEnd'],
-                            fn(Builder $query, $date): Builder => $query->whereDate('test_date', '<=', $date),
-                        );
-                }),
+                    ->label('Survey date range')
+                    ->schema([
+                        DatePicker::make('surveyStart')
+                            ->label('Survey start date'),
+                        DatePicker::make('surveyEnd')
+                            ->label('Survey end date'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['surveyStart'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('test_date', '>=', $date),
+                            )
+                            ->when(
+                                $data['surveyEnd'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('test_date', '<=', $date),
+                            );
+                    }),
             ])
             ->deferFilters(false)
-            ->paginated([10, 25, 50, 'all'])
+            ->paginated([10, 50, 'all'])
             ->recordActions([
                 TableViewAction::make(),
                 TableEditAction::make(),
